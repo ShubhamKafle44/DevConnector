3487a4ff47cbec7429efdbd7538b5ed16dbb2999
ShubhamKafle44 <shubhamkafle44@gmail.com>
2024-07-20 10:18:54 -0500

    initial Routing

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const connectDB = require('./config/db')
 const app =express();
 
 connectDB()
+
 // Init middleware
 app.use(express.json({extended: false}));
 
@@ -12,7 +13,7 @@ app.use(express.json({extended: false}));
 //Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/posts' , require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000
